07ded119cb20a44d446d062a46759e046f12cfe7
PythonProgrammer345 <74526339+RandomPythonProgrammer@users.noreply.github.com>
2023-03-15 13:41:04 -0500

    j

--- main.py
+++ main.py
@@ -8,24 +8,37 @@ class App(pyglet.window.Window):
     def __init__(self):
         super(App, self).__init__(500, 500)
         self.batch = pyglet.graphics.Batch()
-        self.blue = pyglet.shapes.Rectangle(0, 0, 250, 250, batch=self.batch, color=(0, 0, 255, 255))
-        self.bluet = pyglet.text.Label()
-        self.yellow = pyglet.shapes.Rectangle(250, 0, 250, 250, batch=self.batch, color=(255, 255, 0, 255))
-        self.red = pyglet.shapes.Rectangle(250, 250, 250, 250, batch=self.batch, color=(255, 0, 0, 255))
+
         self.green = pyglet.shapes.Rectangle(0, 250, 250, 250, batch=self.batch, color=(0, 255, 0, 255))
+        self.greent = pyglet.text.Label(text='400', x=0, y=250, batch=self.batch, color=(0, 0, 0, 255))
+
+        self.red = pyglet.shapes.Rectangle(250, 250, 250, 250, batch=self.batch, color=(255, 0, 0, 255))
+        self.redt = pyglet.text.Label(text='1000', x=250, y=250, batch=self.batch, color=(0, 0, 0, 255))
+
+        self.blue = pyglet.shapes.Rectangle(0, 0, 250, 250, batch=self.batch, color=(0, 0, 255, 255))
+        self.bluet = pyglet.text.Label(text='5000', x=0, y=0, batch=self.batch, color=(0, 0, 0, 255))
+
+        self.yellow = pyglet.shapes.Rectangle(250, 0, 250, 250, batch=self.batch, color=(255, 255, 0, 255))
+        self.yellowt = pyglet.text.Label(text='10000', x=250, y=0, batch=self.batch, color=(0, 0, 0, 255))
+
         self.audioThread: threading.Thread = None
         self.elapsed = 15
         self.freq = 0
-        self.guess = 0
+        self.guess = None
+
+        self.player = pyglet.media.player.Player()
+        self.player.queue(pyglet.media.synthesis.Sine(15, self.freq))
 
     def render(self, dt):
         self.batch.draw()
         if self.elapsed >= 15:
-            self.freq = random.choice([400, 1000, 5000, 15000])
+            self.freq = random.choice([400, 1000, 5000, 10000])
             if random.random() < 0.5:
-                self.freq = random.random() * 14800 + 200
+                self.freq = random.randint(200, 10000)
 
-            pyglet.media.synthesis.Sine(15, self.freq).play()
+            self.player.queue(pyglet.media.synthesis.Sine(15, self.freq))
+            self.player.next_source()
+            self.player.play()
             self.elapsed = 0
         self.elapsed += dt
 
@@ -66,7 +79,7 @@ class App(pyglet.window.Window):
 
     def yellowp(self):
         self.yellow.color = (100, 100, 0, 255)
-        self.guess = 15000
+        self.guess = 10000
 
 
 if __name__ == '__main__':
